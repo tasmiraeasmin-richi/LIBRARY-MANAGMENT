e5259c473e60be41ef64932528f613768e544c8e
Tasmira Easmin <tasmiraeasmin@gmail.com>
2026-09-17 22:23:07 +0600

    fix database connection

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.scruppssessysthjgbay:018easmin1028@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
