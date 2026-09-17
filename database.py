2013e6b5b6b9d1f28bc5cde359b32900a5b1dd78
Tasmira Easmin <tasmiraeasmin@gmail.com>
2026-09-17 21:37:45 +0600

    postgresql added

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./library.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.scruppssessysthjgbay:018easmin1028@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
